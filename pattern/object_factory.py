595544a0ae84d97312ea0b9734e396c2309ce2a6
Michal Strzelecki <michael.strzelecki23@gmail.com>
2022-10-06 12:21:59 +0100

    adding object factory

---
+++ pattern/object_factory.py
@@ -0,0 +1,15 @@
+from distutils.command.build import build
+
+
+class ObjectFactory:
+    def __init__(self):
+        self._builders = {}
+
+    def register_builder(self, key, builder):
+        self._builders[key] = builder
+
+    def crearte(self, key, **kwargs):
+        builder = self._builders.get(key)
+        if not builder:
+            raise ValueError(key)
+        return builder(**kwargs)
